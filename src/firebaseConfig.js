f8fd6c469fd50815661d7f01f5044a31eadc294b
ariel biton <arielbiton03@gmail.com>
2024-06-18 15:47:44 +0300

    server_connected

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -17,8 +17,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 const db = getFirestore(app);
+
+const analytics = getAnalytics(app);
 
 export { db };
